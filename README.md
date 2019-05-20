6503decfba62d1f7a162ee5655cef03b9ffc29f5
Tiit Ülejõe <tiit@infokiir.ee>
2019-05-20 09:39:59 +0300

    My sample code for reading and writing Excel cell values using Microsoft Excel C API.

--- README.md
+++ README.md
@@ -1,25 +1,8 @@
 # xlltemplate
 
-Template for Excel add-ins
+This is forked https://github.com/keithalewis/xlltemplate repository.
 
-Fork or clone the repository. Set the `Configuration` (Debug or Release) and `Platform` (x86 or x64) and build the `xlltemplate` project.  
-The platform must be set to the appropriate version of Excel: x86 for 32-bit or x64 for 64-bit.
+My simple sample shows how to read and write Excel cell values using C++. 
+It is based on [Microsoft Excel C API](https://docs.microsoft.com/en-us/office/client-developer/excel/programming-with-the-c-api-in-excel)
 
-## Debugging
-
-Set the configuration to `Debug|x86` if using 32-bit Excel or `Debug|x64` for 64-bit Excel.
-Set breakpoints by clicking on the left boarder at the line you want to stop at, the hit `F5` to 
-build and start debugging.
-
-## Documentation
-
-Instantiate an `AddIn xai_anyname(Documentation(LR"(Documentation goes here)"));` object to create a 
-[Sandcastle Help File Builder](https://github.com/EWSoftware/SHFB) project file (`.shfbproj`) in the
-Debug folder of your project. Put the `.chm` file it creates in the same location as the `.xll` add-in to
-integrate with Excel's Insert Function [Help on this function](https://support.office.com/en-us/article/Insert-Function-74474114-7C7F-43F5-BEC3-096C56E2FB13).
-
-## TODO
-
-To create project template: Project|Export Template...
-
-Create installer that adds a Tools|External Tools... To run cmd.exe /c %1 to be used with .bat files.
+For more information please look at my video: <https://youtu.be/_90HgVo1D5c> (in Estonian)
--- xlltemplate.cpp
+++ xlltemplate.cpp
@@ -24,6 +24,17 @@ AddIn xai_function(
 Documentation on XLL.FUNCTION goes here.
     )")
 );
+
+void set_cell_address(XLOPER12& cell_ref, RW rw, COL col)
+{
+	cell_ref.xltype = xltypeSRef;
+	cell_ref.val.sref.count = 1;
+	cell_ref.val.sref.ref.rwFirst = rw;
+	cell_ref.val.sref.ref.rwLast = rw;
+	cell_ref.val.sref.ref.colFirst = col;
+	cell_ref.val.sref.ref.colLast = col;
+}
+
 // Calling convention *must* be WINAPI (aka __stdcall) for Excel.
 LPOPER WINAPI xll_function(double x)
 {
@@ -32,8 +43,35 @@ LPOPER WINAPI xll_function(double x)
 	static OPER result;
 
 	try {
-		ensure(x >= 0);
-		result = sqrt(x); // OPER's act like Excel cells.
+		//Read cell B1 value:
+
+		XLOPER12 reading_parameters;
+		reading_parameters.xltype = xltypeInt;
+		reading_parameters.val.w = 5; // contents of cell as number
+
+		XLOPER12 reading_cell_ref;
+		set_cell_address(reading_cell_ref, 1, 1);
+
+		XLOPER12 result_value;
+		Excel12(xlfGetCell, &result_value, 2, &reading_parameters, &reading_cell_ref);
+
+		//Outputting result to cell F6
+
+		XLOPER12 out_cell_ref;
+		std::wstring hello_msg = L"Hello, world. Cell B2 value is:";
+		if (result_value.xltype == xltypeStr)
+		{
+			auto len = result_value.val.str[0];
+			std::wstring value(result_value.val.str + 1, len);
+			hello_msg += value;
+		}
+		if (result_value.xltype == xltypeNum)
+			hello_msg += std::to_wstring(result_value.val.num);
+		OPER12 xValue(hello_msg);
+		set_cell_address(out_cell_ref, 5, 5);
+		Excel12(xlSet, nullptr, 2, &out_cell_ref, &xValue);
+
+		result = x;
 	}
 	catch (const std::exception& ex) {
 		XLL_ERROR(ex.what());
